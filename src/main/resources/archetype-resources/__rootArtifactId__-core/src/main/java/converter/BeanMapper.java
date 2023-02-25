b8013a7a0d38459f62624ae59c1aeecde3d7fd93
frank.huang <2489868503@qq.com>
2023-02-25 12:33:25 +0800

    集成mapstract

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/converter/BeanMapper.java
@@ -0,0 +1,14 @@
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
+package ${package}.converter;
+
+import org.mapstruct.Mapper;
+
+/**
+ * @author: frank.huang
+ * @date: 2023-02-25 12:29
+ */
+@Mapper(componentModel = "spring")
+public class BeanMapper {
+}
